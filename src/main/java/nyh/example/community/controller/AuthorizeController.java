a87c0162b5d17b375c91cfaac4110d1fbe16f338
unknown <nyh5520@163.com>
2019-07-06 15:01:58 +0800

    extract github config from code to properties

--- src/main/java/nyh/example/community/controller/AuthorizeController.java
+++ src/main/java/nyh/example/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import nyh.example.community.dto.AccessTokenDto;
 import nyh.example.community.dto.GithubUser;
 import nyh.example.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,16 +16,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
 //   @RequestParam参数的接收
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("5c23682409d8d80165d4");
-        accessTokenDto.setClient_secret("65fbe3320bcba684b783d70525ea1a6cc8787511");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
 //        拿到accesstoken
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
